8b6fb47cc6fba8d1f9ef80f1e108dbad9c33ee81
OlgaKrzy <89542@student.ue.poznan.pl>
2023-04-17 19:53:11 +0200

    Zadanie 1.9

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -53,3 +53,13 @@ studenci.sort(key= lambda k: k.split()[-1])
 print("Alfabetyczna lista studentow wynosi: ")
 for student in studenci:
     print(student)
+
+# zadanie 1.9
+
+studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
+liczba_n = 0
+for student in studenci:
+    last_name=student.split()[-1]
+    if last_name.startswith("N"):
+        liczba_n += 1
+print("Liczba studentow na N wynosi: " + str(liczba_n))
