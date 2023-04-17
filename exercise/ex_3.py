b1db1327a6c6dd58d8827ca076ea16f0d3f648a0
Martyna Kolenda <89539@student.ue.poznan.pl>
2023-04-17 12:45:45 +0200

    rownoleglobok, romb i trapez

--- exercise/ex_3.py
+++ exercise/ex_3.py
@@ -39,15 +39,18 @@ import math
 pi=math.pi
 def kolo(promien_kola):
 
-obwod_kolo=2 * pi * promien_kola
-pole_kola= promien_kola * promien_kola * pi
-return obwod_kolo, pole_kola
+    obwod_kolo=2 * pi * promien_kola
+    pole_kola= promien_kola * promien_kola * pi
+    return obwod_kolo, pole_kola
 
 
 assert trojkat(10, 15, 16, 8) == (41, 40)
 assert kwadrat(20) == (80, 400)
 assert prostokat(12, 10) == (44, 120)
 assert rownoleglobok(6, 5, 2) == (22, 12)
+assert rownoleglobok(10, 8, 7) == (36, 70)
 assert romb(10, 5) == (40, 50)
-assert trapez(10, 15, 7, 14, 2) == (45, 25)
+assert romb(2, 3) == (8, 6)
+assert trapez(10, 15, 7, 14, 2) == (46, 25)
+assert trapez(9, 11, 6, 8, 3) == (34, 30)
 # TODO na koniec! dopisz 2 testy dla kola i dla kazdej innej figury po jednym dodatkowym tescie
