4359a2973b58615cd0af4c8b2b923300d96a4456
OlgaKrzy <89542@student.ue.poznan.pl>
2023-04-17 19:48:45 +0200

    Zadanie 1.3

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -10,3 +10,9 @@ print(hello.format(student))
 student = input("Wpisz swoje imie")
 hello = "Hello {}"
 print(hello.format(student))
+
+# zadanie 1.3
+
+studenci = ["Ania", "Kuba", "Piotr", "Jan"]
+liczba_studentow = str(len(studenci))
+print("Liczba studentow wynosi: " + liczba_studentow)
